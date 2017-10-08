0cf2df28c8c52ad312f240845ef14de0f26e4de5
PhuongHieu <fuhi.dev@gmail.com>
2017-10-08 11:28:36 +0700

    them thoi gian

--- modules/db.js
+++ modules/db.js
@@ -1,22 +1,23 @@
-const { Client,Pool } = require('pg')
+const { Client, Pool } = require('pg')
 const config = {
   host: 'ditagis.com',
   port: 5432,
   user: 'postgres',
   password: 'ditagis@2017',
-  database:'test'
+  database: 'test'
 };
 const client = new Client(config);
 client.connect();
-var sendEmail = function(email){
-    return new Promise((resolve, reject) => {
-        let query = `INSERT INTO contact(email)VALUES ('${email}');`
-        
-        client.query(query)
-          .then(res => {
-            resolve(res)
-          })
-          .catch(e => {reject(e)})
-      });
+var sendEmail = function (email) {
+  return new Promise((resolve, reject) => {
+    let time = new Date().getTime();
+    let query = `INSERT INTO contact(email,thoigian)VALUES ('${email}',CURRENT_TIMESTAMP);`
+
+    client.query(query)
+      .then(res => {
+        resolve(res)
+      })
+      .catch(e => {console.log(e); reject(e) })
+  });
 }
 exports.sendEmail = sendEmail;
