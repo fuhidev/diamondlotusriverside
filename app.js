a874540ab9694cfdfeb8e6cea1229168c7aa0d47
PhuongHieu <fuhi.dev@gmail.com>
2017-10-08 09:38:55 +0700

    ok

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,7 +31,10 @@ app.get("/gioi-thieu", function (req, res, next) {
   res.render('about', { title: 'Express' });
 })
 app.post('/send-email',function(req,res,next){
-  console.log(req.body['email']);
+  const name = req.body['name'],email = req.body['email'],message = req.body['message'];
+})
+app.post('/contact',function(req,res,next){
+  
 })
 // app.use('/', index);
 // app.use('/users', users);
